aadea3fb1f3a696ca73a862240d409486215dc6a
Wenyu Shi <d00198805@student.dkit.ie>
2020-09-17 15:54:21 +0100

    17/09/2020

--- src/day16_09/StrStr_28.java
+++ src/day16_09/StrStr_28.java
@@ -9,7 +9,7 @@ public class StrStr_28 {
          * 链接：https://leetcode-cn.com/problems/implement-strstr
          * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
          */
-        System.out.println(strStr2("hello", "o"));
+        System.out.println(strStr2("", ""));
     }
 
     public static int strStr(String haystack, String needle) {
